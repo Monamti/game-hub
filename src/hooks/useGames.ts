1933ce65bbe62dbd8057b5324dd20e68aef09b2d
mona.mti <mna.mtiafra@yahoo.com>
2023-05-14 13:49:38 +0330

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,20 +24,28 @@ export interface Game {
 const useGames = () =>{
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading , setLoding] = useState(true);
   
     useEffect(() => {
         const controller = new AbortController();
+
+      setLoding(true);
       apiClient
         .get<FetchGamesResponse>("/games" , {signal:controller.signal})
-        .then((resp) => setGames(resp.data.results))
+        .then((resp) => {
+          setGames(resp.data.results)
+          setLoding(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err.message)});
+            setError(err.message)
+          setLoding(false)
+        });
 
         return ()=> controller.abort();
     } , []);
 
-    return {games , error}
+    return {games , error , isLoading}
 }
 
 
